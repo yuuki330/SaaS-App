ddd9ec4bbe954ba82fea9f67f054156ff90847f8
yuuki330 <katsuyu107@gmail.com>
2024-07-22 07:58:42 +0000

    Updated views change order

--- src/cfehome/views.py
+++ src/cfehome/views.py
@@ -10,15 +10,13 @@ def home_page_view(request, *args, **kwargs):
     qs = PageVisit.objects.all()
     page_qs = PageVisit.objects.filter(path=request.path)
     my_title = "My Page"
+    html_template = "home.html"
     my_context = {
         "page_title": my_title,
         "page_visit_count": page_qs.count(),
         "percent": (page_qs.count() * 100.0) / qs.count(),
         "total_visit_count": qs.count()
     }
-    path = request.path
-    print("path", path)
-    html_template = "home.html"
     PageVisit.objects.create(path=request.path)
     return render(request, html_template, my_context)
 
