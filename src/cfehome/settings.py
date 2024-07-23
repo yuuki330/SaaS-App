701cd66f55bafab4924495e0191500e82a1ba2ef
yuuki330 <katsuyu107@gmail.com>
2024-07-23 03:52:50 +0000

    pycacheの削除&環境変数の設定

--- src/cfehome/settings.py
+++ src/cfehome/settings.py
@@ -10,8 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+from decouple import config
 from pathlib import Path
-import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,11 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!^ba3lk8qd_u3a)g9u0h&z@58msq9y2wa996hu&ru*trjts0-5'
+SECRET_KEY = config("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = str(os.environ.get("DJANGO_DEBUG")).lower() == "true"
+# DEBUG = str(os.environ.get("DJANGO_DEBUG")).lower() == "true"
  
+DEBUG = config("DJANGO_DEBUG", cast=bool)
 print("DEBUG", DEBUG, type(DEBUG))
 
 ALLOWED_HOSTS = [
