6fb26275dfb3b71bf3a170d1694251fb5fed2758
yuuki330 <katsuyu107@gmail.com>
2024-07-23 02:12:35 +0000

    Change percent

--- src/cfehome/views.py
+++ src/cfehome/views.py
@@ -22,7 +22,7 @@ def about_view(request, *args, **kwargs):
     my_context = {
         "page_title": my_title,
         "page_visit_count": page_qs.count(),
-        "percent": (page_qs.count() * 100.0) / qs.count(),
+        "percent": percent,
         "total_visit_count": qs.count()
     }
     PageVisit.objects.create(path=request.path)
