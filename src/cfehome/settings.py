1ff6b8192b8dc6bc1499f62fa7232f1666f8a1a5
yuuki330 <katsuyu107@gmail.com>
2024-07-23 07:06:46 +0000

    Updated build args

--- src/cfehome/settings.py
+++ src/cfehome/settings.py
@@ -94,8 +94,8 @@ DATABASES = {
     }
 }
 
-CONN_MAX_AGE = config("CONN_MAX_AGE", cast=int, default=30)
-DATABASE_URL = config("DATABASE_URL", cast=str)
+CONN_MAX_AGE = config("CONN_MAX_AGE", cast=int, default=300)
+DATABASE_URL = config("DATABASE_URL", default=None)
 
 if DATABASE_URL is not None:
     import dj_database_url
