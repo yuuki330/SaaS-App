e2e079a0e451b7cdaf3d1af94c7aef0acc29cff2
yuuki330 <katsuyu107@gmail.com>
2024-07-26 07:15:08 +0000

    navbarの改善

--- src/cfehome/urls.py
+++ src/cfehome/urls.py
@@ -21,7 +21,7 @@ from .views import home_view, about_view
 from auth import views as auth_views
 
 urlpatterns = [
-    path("", home_view),
+    path("", home_view, name='home'),
     path("about/", about_view),
     path("login/", auth_views.login_view),
     path("register/", auth_views.register_view),
